adbda535b3f9c13bb9968565a9a49d988e08cec5
Thuringud <rom1234593@yandex.ru>
2021-11-26 15:00:54 +0300

    tasks 2.1-2.7

--- 02-components/07-PageMeetup/PageMeetup.js
+++ 02-components/07-PageMeetup/PageMeetup.js
@@ -1,7 +1,8 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import { fetchMeetupById } from './meetupService.js';
+import MeetupView from '../06-MeetupView/MeetupView.js';
 
 export default defineComponent({
   name: 'PageMeetup',
@@ -9,18 +10,57 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupView,
+  },
+
+  props: {
+    meetupId: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      meetup: null,
+      meetupIsLoaded: false,
+      meetupLoadedError: null,
+    };
+  },
+
+  watch: {
+    meetupId: {
+      handler: function (meetupId) {
+        this.updateMeetup(meetupId);
+      },
+      immediate: true,
+    },
+  },
+
+  methods: {
+    updateMeetup(meetupId) {
+      this.meetupIsLoaded = false;
+      this.meetupLoadedError = '';
+
+      fetchMeetupById(meetupId)
+        .then((meetup) => {
+          this.meetup = meetup;
+          this.meetupIsLoaded = true;
+        })
+        .catch((e) => {
+          this.meetupLoadedError = e.message;
+        });
+    },
   },
 
   template: `
-    <div class="page-meetup">
-      <!-- meetup view -->
-
-      <ui-container>
-        <ui-alert>Загрузка...</ui-alert>
-      </ui-container>
-
-      <ui-container>
-        <ui-alert>error</ui-alert>
-      </ui-container>
-    </div>`,
+   <div class="page-meetup">
+     <MeetupView v-if="meetupIsLoaded" :meetup="meetup"/>
+     <ui-container v-else-if="!meetupIsLoaded && !meetupLoadedError">
+       <ui-alert>Загрузка...</ui-alert>
+     </ui-container>
+     <ui-container v-else>
+       <ui-alert>{{ meetupLoadedError }}</ui-alert>
+     </ui-container>
+   </div>`,
 });
